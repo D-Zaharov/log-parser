d48bde511b43fb54b1eea1252a1f74b3c4b22446
D-Zaharov <Zdaniil1512@gmail.com>
2019-08-12 14:24:44 +0300

    Добавлен главный файл, в котором собрана вся программа

---
+++ src/index.py
@@ -0,0 +1,9 @@
+﻿import download_func
+import findsearchquery
+
+for i in range (1, 51):
+    link_to_file = str('https://api.raan.garagemca.org/syslog.' + i + '.gz')
+    list_of_path = download_func.path_of_files(link_to_file)
+    for element in list_of_path:
+        result = findsearchquery.finder(element)
+        print(result)
