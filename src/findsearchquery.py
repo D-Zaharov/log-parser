8725998e6055c83d9c2368ba7d0ecd90d149ab3b
D-Zaharov <Zdaniil1512@gmail.com>
2019-08-12 14:40:24 +0300

    moved findsearchquery to src folder

---
+++ src/findsearchquery.py
@@ -0,0 +1,24 @@
+import subprocess
+import urllib.parse
+import re
+
+def finder(pathtofile):
+    cmd = f"cat {pathtofile} | grep search%5Bquery%5D="
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    reslt = process.communicate()[0]
+    listofall = []
+    reslt_as_string_list = str(reslt).split("\\n")
+
+    for s in reslt_as_string_list:
+        try:
+
+            f = urllib.parse.unquote(s)
+            regex  = 'search\[query\]=([^&]+)'
+            res = re.search(regex, f)
+            listofall.append([res.group(1), f])
+
+        except AttributeError:
+            listofall.append("Nothing found")
+            print("Query for number" + " " + s + " " + "not found :(")
+    return(listofall)
+
