c8736a0fe537c1902aba1f0232c31ec7bd02c56d
D-Zaharov <Zdaniil1512@gmail.com>
2019-08-22 16:55:35 +0300

    Добавлены потоки

--- src/index.py
+++ src/index.py
@@ -1,9 +1,24 @@
 ﻿import download_func
 import findsearchquery
+from concurrent.futures import ThreadPoolExecutor
 
-for i in range (2, 3):
-    link_to_file = 'https://api.raan.garagemca.org/syslog.' + str(i) + '.gz'
-    print("Downloading " + link_to_file)
+threads_count = 4
+
+def get_list (list_of_links):
     path = download_func.path_of_file(link_to_file)
     result = findsearchquery.finder(path)
-    print(result)
+    return(result)
+
+def execute_threads(urls):
+    with ThreadPoolExecutor(max_workers=threads_count) as executor:
+      return executor.map(get_list, urls, timeout=120)
+
+
+list_of_links = []
+for i in range (2, 3):
+    link_to_file = 'https://api.raan.garagemca.org/syslog.' + str(i) + '.gz'
+    list_of_links.append(link_to_file)
+
+results = execute_threads(links.list_of_links)
+
+print(results)
